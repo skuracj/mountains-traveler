9b65efd0cb4e5d576c845d3de6b7cd17efc07916
skuracj <73294501+skuracj@users.noreply.github.com>
2020-11-25 22:22:39 +0100

    Settings modal added + icon in app.componeent (#16)

---
+++ FE/mountainTraveler/src/app/components/settings-modal/settings-modal.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {ModalController} from '@ionic/angular';
+
+@Component({
+  selector: 'app-settings-modal',
+  templateUrl: './settings-modal.component.html',
+  styleUrls: ['./settings-modal.component.scss'],
+})
+export class SettingsModalComponent implements OnInit {
+
+  constructor(private modalController: ModalController) { }
+
+  ngOnInit() {}
+
+  dismissModal() {
+    // using the injected ModalController this page
+    // can "dismiss" itself and optionally pass back data
+    this.modalController.dismiss({
+      'dismissed': true
+    });
+  }
+
+}
