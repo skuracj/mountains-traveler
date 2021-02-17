e0de2f949c2bd10d0ae42886880150646eae0b71
skura.cj <skura.cj@gmail.com>
2021-02-17 20:30:56 +0100

    Removed jenkins folder

--- jenkins-playground/install-jenkins.sh
+++
@@ -1,3 +0,0 @@
-docker run -p 8080:8080 -p 50000:50000 -v jenkins-data:/var/jenkins_home -d --name jenkins-script jenkins/jenkins:lts
-echo 'Jenkins installed'
-echo 'You should now be able to access jenkins at: http://localhost:8080'
